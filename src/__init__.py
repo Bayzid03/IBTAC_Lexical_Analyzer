020a2e490b7295ece04c89b109a154e17a62caa5
Bayzid03 <143407674+Bayzid03@users.noreply.github.com>
2025-12-15 21:31:09 +0600

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+IBTAC Lexer Package
+This file makes the src directory a Python package.
+"""
+
+# This file is intentionally left empty.
+# Its presence makes Python treat the directory as a package.
